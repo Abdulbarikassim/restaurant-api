1e005724d1805bee394f3ebf60db76c7e7f0cd9f
Abdulbarikassim <abdicsxhaka10@gmail.com>
2026-05-07 19:27:59 +0300

    Feat: Added menu, order data models, API endpoints
    - Added the MenuItem struct - which will contain all the menu items.
    - OrderItem struct - which will contain each order item.
    - Order - contains all the order items for a customer, status and the
      total price
    - Added the API endpoints for the menu management and order processing.

--- main.go
+++ main.go
@@ -4,10 +4,43 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"github.com/gin-gonic/gin"
 
-	"github.com/joho/godotenv"
+	"net/http"
 	"github.com/Abdulbarikassim/restaurant-api/db"
+	"github.com/joho/godotenv"
 )
+
+// menu item
+
+type MenuItem struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Category string `json:"category"`
+	Price float64 `json:"price"`
+	Available bool `json:"available"`
+}
+
+// order Item
+
+type OrderItem struct {
+	MenuItemId int `json:"menu_item_id"`
+	Name string `json:"name"`
+	Quantity int `json:"quantity"`
+	Price float64 `json:"price"`
+}
+
+// All the order items
+
+type Order struct {
+	ID 	string `json:"id"`
+	CustomerPhone string `json:"customer_phone"`
+	Status string `json:"status"`
+	TotalAmount float64 `json:"total_amount"`
+	Items []OrderItem 	`json:"items"`
+}
+
 
 func main() {
 
@@ -23,14 +56,13 @@ func main() {
 		log.Fatal("DATABASE_URL is not found")
 	}
 
-	database , err := db.ConnectDB()
+	database, err := db.ConnectDB()
 
-	if 	err != nil {
+	if err != nil {
 		log.Fatal("DB connection failed: ", err)
 	}
 
 	defer database.Close()
-
 
 	fmt.Println("Connected to postgreSQL successfully")
 
@@ -43,5 +75,27 @@ func main() {
 	fmt.Println("App is running")
 
 
-}
+	router := gin.Default()
 
+	// menu routes
+
+	router.GET("/menu", getMenuItems)
+	router.GET("/menu/:id", getMenuById)
+	router.PUT("/menu/:id", updateMenu)
+	router.POST("/menu", postNewMenu)
+
+
+	// order routes
+
+	router.GET("/orders", getOrders)
+	router.GET("/orders/:id", getOrderById)
+	router.POST("/orders", postNewOrder)
+	router.PUT("/order/:id/status",updateOrderStatus)
+
+
+
+
+
+
+
+}
