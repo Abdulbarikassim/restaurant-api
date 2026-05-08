ac923ad8696b747a3a22d603d033a6cbd5ba9463
Abdulbarikassim <abdicsxhaka10@gmail.com>
2026-05-08 23:38:21 +0300

    Func: Added getItemById
    This functions gets the item in the menu by id.

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+
 	"github.com/gin-gonic/gin"
 
 	"net/http"
+
 	"github.com/Abdulbarikassim/restaurant-api/db"
 	"github.com/joho/godotenv"
 )
@@ -41,6 +44,7 @@ type Order struct {
 	Items []OrderItem 	`json:"items"`
 }
 
+var database *sql.DB
 
 func main() {
 
@@ -56,7 +60,7 @@ func main() {
 		log.Fatal("DATABASE_URL is not found")
 	}
 
-	database, err := db.ConnectDB()
+	database, err = db.ConnectDB()
 
 	if err != nil {
 		log.Fatal("DB connection failed: ", err)
@@ -80,22 +84,191 @@ func main() {
 	// menu routes
 
 	router.GET("/menu", getMenuItems)
-	router.GET("/menu/:id", getMenuById)
-	router.PUT("/menu/:id", updateMenu)
-	router.POST("/menu", postNewMenu)
+	//router.GET("/menu/:id", getMenuById)
+	router.PUT("/menu/:id", updateMenuItem)
+	router.POST("/menu",createMenuItem )
 
 
 	// order routes
 
-	router.GET("/orders", getOrders)
-	router.GET("/orders/:id", getOrderById)
-	router.POST("/orders", postNewOrder)
-	router.PUT("/order/:id/status",updateOrderStatus)
+	//router.GET("/orders", getOrders)
+	//router.GET("/orders/:id", getOrderById)
+	//router.POST("/orders", postNewOrder)
+	//router.PUT("/order/:id/status",updateOrderStatus)
 
-
-
-
+	router.Run(":8080")
 
 
 
 }
+
+
+// logic to get menu items
+
+func getMenuItems(c *gin.Context) {
+	rows, err := database.Query(`SELECT * FROM menu_items`)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	defer rows.Close()
+
+	var items []MenuItem
+
+	for rows.Next() {
+
+		var item MenuItem
+
+		err := rows.Scan(
+			&item.ID,
+			&item.Name,
+			&item.Description,
+			&item.Category,
+			&item.Price,
+			&item.Available,
+		)
+
+		if err != nil {
+			continue
+		}
+
+		items =  append(items, item)
+	}
+
+	c.JSON(http.StatusOK, items)
+}
+
+func createMenuItem(c *gin.Context) {
+	var item MenuItem
+
+
+	if err  := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+
+	query := `
+		INSERT INTO menu_items
+		(name, description, category,price, available)
+		VALUES ($1,$2,$3,$4,$5)
+		RETURNING id
+	`
+
+	err := database.QueryRow(
+		query,
+		item.Name,
+		item.Description,
+		item.Category,
+		item.Price,
+		item.Available,
+	).Scan(&item.ID)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,gin.H{
+			"error" : err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, item)
+}
+
+// logic to update MenuItem
+
+
+func updateMenuItem(c *gin.Context) {
+
+	id := c.Param("id")
+
+	var item MenuItem
+
+	if err := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	query := `
+		UPDATE menu_items
+		SET
+			price = $1,
+			available = $2
+		WHERE id = $3
+	`
+
+	_, err := database.Exec(
+		query,
+		item.Price,
+		item.Available,
+		id,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "menu item updated",
+	})
+
+}
+
+
+// getMenuById function
+
+func getMenuById(c *gin.Context){
+
+	id := c.Param("id")
+
+	var item MenuItem
+
+	query := `
+	SELECT id, name, description,category, price, available FROM menu_items WHERE id = $1
+	`
+
+	err := database.QueryRow(query, id).Scan(
+		&item.ID,
+		&item.Name,
+		&item.Description,
+		&item.Category,
+		&item.Price,
+		&item.Available,
+	)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows{
+			c.JSON(http.StatusNotFound,gin.H{
+				"error": "menu item not found",
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError,gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK,item)
+}
+
+
+
+
+
+
+
